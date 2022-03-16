6c8e840c2fd318e65d6110cd0d6d3ad85bf4091e
mandeep thakur <tmanu76269@gmail.com>
2022-03-16 10:35:37 +0530

    web development course

---
+++ java script/java7.js
@@ -0,0 +1,22 @@
+console.log("This is tutorial 7");
+
+function greet(name,gm){
+    console.log(gm+" "+name);
+    console.log(name+" is a good boy");
+}
+
+// function is a block of code disigned to perform a particular task
+let name = "mandeep";
+let name1 = "aman";
+let name2= "suraj";
+let name3 = "laddi";
+let gm = "Good Morning";
+greet(name, gm);
+greet(name1, gm);
+greet(name2, gm);
+greet(name3, gm);
+
+// console.log(name + " is a good boy");
+// console.log(name1 + " is a good boy");
+// console.log(name2+ " is a good boy");
+// console.log(name3 + " is a good boy");
---
+++ thakur vs code.html/java7.js
@@ -0,0 +1,27 @@
+console.log('this is tutorial 53');
+function greet(name,greettext1="greetings to java script"){
+    let name1 ="Name1"
+  console.log( greettext1  +  " "  +  name);
+  console.log( name + " is a good boy");
+}
+function sum (a ,b,c){
+  let d=a+b+c;
+  return d;
+}
+
+
+let name ="mandeep";
+let name1 ="Suraj";
+let name2 ="laddi";
+let name3="arjun";
+let greettext1 =" good morning"
+greet(name,greettext1);
+greet(name1,greettext1);
+greet(name2,greettext1);
+// greet(name3);
+let returnval = sum(1+2+3);
+console.log(returnval)
+// console.log( name +" is a good boy")
+// console.log( name1+" is a good boy")
+// console.log( name2 +" is a good boy")
+// console.log( name3+" is a good boy")
